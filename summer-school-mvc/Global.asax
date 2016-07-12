d8ab4bc1f60b91905f755ed13b68b77caf81fd7c
Prince <princemwilliams@gmail.com>
2016-07-12 09:39:31 -0400

    Got everything done. Even added some styling. Having minor issues with the jumbotron pic.

---
+++ summer-school-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="summer_school_mvc.MvcApplication" Language="C#" %>
---
+++ summer-school-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(summer_school_mvc.Startup))]
+namespace summer_school_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
